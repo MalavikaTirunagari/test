03b9cce6c915c30eb17a957d74793c0d2fb2d279
Malavika <malavikatirunagari07@gmail.com>
2024-11-20 12:51:54 +0530

    add

--- Prime.java
+++ Prime.java
@@ -1,17 +1,6 @@
 public class Prime {
-    public static boolean isPrime(int number) {
-        if (number <= 1) {
-            return false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    
+   
+
     public static void main(String[] args) {
         int number = 29; // You can change this number to test other values
         boolean isPrime = true;
